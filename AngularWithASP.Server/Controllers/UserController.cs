3a24675ce17f3dba657c4b600cab53935a1ba646
Bastien <chachussr@gmail.com>
2025-03-12 09:21:11 +0100

    Création de la structure de fichier

---
+++ AngularWithASP.Server/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private static List<string> users = new List<string> { "Alice", "Bob", "Charlie" };
+
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        return Ok(users);
+    }
+
+    [HttpPost]
+    public IActionResult AddUser([FromBody] string user)
+    {
+        users.Add(user);
+        return Ok(users);
+    }
+}
--- AngularWithASP.Server/Program.cs
+++ AngularWithASP.Server/Program.cs
@@ -1,30 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 var app = builder.Build();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
+app.UseCors("AllowAngular");
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
-
 app.Run();
